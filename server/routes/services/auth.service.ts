16dcc8c9da67d86f1204e6bd52254aafd1b87ba1
Daniel304F <Daniel-Fischer304@tutanota.de>
2025-10-01 13:48:01 +0200

    Added Register form

---
+++ server/routes/services/auth.service.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const SECRET = "mysecret" + new Date().getTime();
+
+class AuthService {
+  authenticationMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (res.locals.user) {
+      next();
+    } else {
+      const token = req.cookies["jwt-token"] || "";
+      try {
+        res.locals.user = this.verifyToken(token);
+        next();
+      } catch {
+        res.redirect("/users/sign-in");
+      }
+    }
+  };
+
+  createAndSetToken(userClaimSet: Record<string, unknown>, res: Response) {
+    const token = jwt.sign(userClaimSet, SECRET, {
+      algorithm: "HS256",
+      expiresIn: "1h",
+    });
+    res.cookie("jwt-token", token, { /* httpOnly: true, */ sameSite: "lax" });
+  }
+
+  verifyToken(token: string) {
+    return jwt.verify(token, SECRET);
+  }
+
+  removeToken(res: Response) {
+    res.clearCookie("jwt-token");
+  }
+}
+
+export const authService = new AuthService();
