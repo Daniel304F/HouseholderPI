56f4a4e39ab0003e93b95e5f4b67dfc307286156
Daniel Fischer <daniel-fischer304@tutanota.de>
2026-01-04 17:38:32 +0100

    Edit App.tsx -- Add routes for navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
+import { routes } from "./routes";
 
+const AppRoutes = async () => {
+    return useRoutes(routes);
+}
 
 export const App = () => {
-    return (<BrowserRouter></BrowserRouter>)
+    return (<BrowserRouter><AppRoutes></AppRoutes></BrowserRouter>)
 }
