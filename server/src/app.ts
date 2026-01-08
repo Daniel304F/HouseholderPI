4a96825bdfec5a68e799dc3ae6020902022bb848
Daniel304F <Daniel-Fischer304@tutanota.de>
2026-01-08 21:09:08 +0100

    Add userController, userRoutes, edit app.ts -- added new userRoutes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,12 @@
 import { errorHandler } from "./middlewares/errorHandler.js";
 import express from "express";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 app.use(express.json());
 
 // Routes
-
+app.use("/users", userRoutes);
 // Global error Handler
 app.use(errorHandler);
 
