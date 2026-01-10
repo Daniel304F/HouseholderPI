1496c6b5fd517049322581c664e21241bd3b5d39
Daniel304F <Daniel-Fischer304@tutanota.de>
2026-01-10 13:27:51 +0100

    Add middleware logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,10 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import { loggerMiddleware } from "./middlewares/logger.js";
 
 const app = express();
+app.use(loggerMiddleware);
 app.use(
   cors({
     origin: "http://localhost:5173",
